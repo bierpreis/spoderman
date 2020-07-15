fbe31227e5211a0c552896155b320d84c720e370
Frank <frank.leuchtmann@gmx.de>
2020-07-15 16:19:31 +0200

    Updated comments

--- src/game/Audio.java
+++ src/game/Audio.java
@@ -7,13 +7,12 @@ import java.util.HashMap;
 
 public class Audio {
 
-    private static HashMap<Sound, File> soundMap;
+    private final static HashMap<Sound, File> soundMap;
 
 
     static {
         soundMap = new HashMap<>();
         for (Sound sound : Sound.values()) {
-            //todo: null pointer here!
             try {
                 soundMap.put(sound, new File("./src/resources/sound/" + sound.name() + ".wav"));
             } catch (NullPointerException e) {
@@ -26,37 +25,36 @@ public class Audio {
     }
 
     public static void play(Sound sound) {
-        System.out.println("playing sound " + sound.name());
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true) {
-                    System.out.println("playing sound " + sound.name());
-                    System.out.println("until here..");
-                    AudioInputStream soundStream;
-                    AudioFormat soundFormat;
-                    DataLine.Info info;
-                    Clip soundClip;
+                System.out.println("playing sound " + sound.name());
+                System.out.println("until here..");
+                AudioInputStream soundStream;
+                AudioFormat soundFormat;
+                DataLine.Info info;
+                Clip soundClip;
 
-                    try {
-                        soundStream = AudioSystem.getAudioInputStream(soundMap.get(sound));
-                        soundFormat = soundStream.getFormat();
-                        info = new DataLine.Info(Clip.class, soundFormat);
+                try {
+                    soundStream = AudioSystem.getAudioInputStream(soundMap.get(sound));
+                    soundFormat = soundStream.getFormat();
+                    info = new DataLine.Info(Clip.class, soundFormat);
 
-                        soundClip = (Clip) AudioSystem.getLine(info);
+                    soundClip = (Clip) AudioSystem.getLine(info);
 
-                        soundClip.open(soundStream);
+                    soundClip.open(soundStream);
 
 
-                        soundClip.start();
-                        soundClip.close();
+                    soundClip.start();
+                    soundClip.close();
 
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    //todo: wtf this is never shown?!
-                    System.out.println("finished sound " + sound.name());
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+                //todo: wtf this is never shown?!
+                System.out.println("finished sound " + sound.name());
+
             }
         }).start();
 
