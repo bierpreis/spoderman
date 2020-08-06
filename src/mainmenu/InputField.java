8938bc2647c717712a60fabef65f2927dbe1ddf7
Frank <frank.leuchtmann@gmx.de>
2020-08-06 19:05:09 +0200

    updated todo

--- src/mainmenu/InputField.java
+++ src/mainmenu/InputField.java
@@ -5,6 +5,7 @@ import java.awt.event.KeyListener;
 
 import javax.swing.JTextField;
 
+//todo: remove key listener? seems useless
 class InputField extends JTextField implements KeyListener {
 
     InputField(int cols) {
@@ -14,10 +15,6 @@ class InputField extends JTextField implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        //code= getText();
-        if (e.getKeyCode() == KeyEvent.VK_ENTER)
-            System.out.println("Es war Enter");  //todo wtf what is this?
-
     }
 
     @Override
