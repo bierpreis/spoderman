fa397c57c89007a2c25627df99bae33a5deb51a7
Frank <frank.leuchtmann@gmx.de>
2020-08-06 19:55:31 +0200

    sound is now working

--- src/game/Audio.java
+++ src/game/Audio.java
@@ -2,62 +2,35 @@ package game;
 
 import javax.sound.sampled.*;
 import java.io.File;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
 
 public class Audio {
 
-    private final static HashMap<Sound, File> soundMap;
 
     private static final Logger LOGGER = Logger.getLogger(Game.class.getName());
 
 
-    static {
-        soundMap = new HashMap<>();
-        for (Sound sound : Sound.values()) {
-            try {
-                soundMap.put(sound, new File("./src/resources/sound/" + sound.name() + ".wav"));
-            } catch (NullPointerException e) {
-                LOGGER.warning("NPE Outting " + sound + " and "
-                        + new File("./src/resources/sound/" + sound.name() + ".wav") + " into map ");
-                LOGGER.warning(e.toString());
-            }
-
-        }
-    }
-
     public static void play(Sound sound) {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                LOGGER.fine("playing sound " + sound.name());
-                AudioInputStream soundStream;
-                AudioFormat soundFormat;
-                DataLine.Info info;
-                Clip soundClip;
 
-                try {
-                    soundStream = AudioSystem.getAudioInputStream(soundMap.get(sound));
-                    soundFormat = soundStream.getFormat();
-                    info = new DataLine.Info(Clip.class, soundFormat);
+        URL url = Audio.class.getResource("/resources/sound/" + sound.name() + ".wav");
 
-                    soundClip = (Clip) AudioSystem.getLine(info);
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(url);
 
-                    soundClip.open(soundStream);
+            DataLine.Info info = new DataLine.Info(Clip.class, ais.getFormat());
+
+            Clip clip = (Clip) AudioSystem.getLine(info);
+            clip.open(ais);
+
+            clip.start();
 
 
-                    soundClip.start();
-                    soundClip.close();
-
-                } catch (Exception e) {
-                    LOGGER.severe(e.toString());
-                }
-
-
-            }
-        }).start();
-
+        } catch (Exception e) {
+            LOGGER.severe(e.toString());
+        }
     }
 }
